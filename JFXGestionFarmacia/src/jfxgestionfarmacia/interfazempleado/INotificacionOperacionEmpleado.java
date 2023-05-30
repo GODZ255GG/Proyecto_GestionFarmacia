b3a1d1191c0551fc313766e2c7c043360221f2b2
René García <100447765+GODZ255GG@users.noreply.github.com>
2023-05-30 11:12:52 -0600

    Update INotificacionOperacionEmpleado.java

--- JFXGestionFarmacia/src/jfxgestionfarmacia/interfazempleado/INotificacionOperacionEmpleado.java
+++ JFXGestionFarmacia/src/jfxgestionfarmacia/interfazempleado/INotificacionOperacionEmpleado.java
@@ -5,4 +5,6 @@ public interface INotificacionOperacionEmpleado {
     public void notificarOperacionGuardarEmpleado(String nombreEmpleado);
     
     public void notificarOperacionActualizarEmpleado(String nombreEmpleado);
+    
+    //hola
 }
